1651d77469e00a7f2bd25817b0d1af6e81c1f5c7
“willy <[email protected]>
2023-07-04 21:49:19 +0300

    added a custome error page

---
+++ src/dashboard/pages/errorpage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mt-5 pt-5 text-center text-danger font-weight-bold">
+        <b-container fluid="md" class="mt-5">
+            <b-alert show variant="danger">
+                <h4 class="alert-heading">Oh snap! You got an error! Youre not Authenticated</h4>
+                <p>Login to continue</p>
+                <hr />
+                <b-button class="mt-5" variant="outline" size="lg" block>
+                    <b-link to="/">Login</b-link>
+                </b-button>
+            </b-alert>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
